9e8ecbe9063f054905c67ff31c112184c66f96ff
Raheel Ahmed <raheelahmed017@gmail.com>
2025-06-11 23:33:19 +0500

    Register form changes

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -42,7 +42,7 @@ export default function Register() {
             <form className="flex flex-col gap-6" onSubmit={submit}>
                 <div className="grid gap-6">
                     <div className="grid gap-2">
-                        <Label htmlFor="name">Agency Name</Label>
+                        <Label htmlFor="agency_name">Agency Name</Label>
                         <Input
                             id="agency_name"
                             type="text"
@@ -58,103 +58,109 @@ export default function Register() {
                         <InputError message={errors.agency_name} className="mt-2" />
                     </div>
 
-                    <div className="grid gap-2">
-                        <Label htmlFor="name">Website</Label>
-                        <Input
-                            id="website"
-                            type="text"
-                            required
-                            tabIndex={1}
-                            autoComplete="website"
-                            value={data.website}
-                            onChange={(e) => setData('website', e.target.value)}
-                            disabled={processing}
-                            placeholder="Website"
-                        />
-                        <InputError message={errors.website} className="mt-2" />
-                    </div>
-                    <div className="grid gap-2">
-                        <Label htmlFor="name">Domain</Label>
-                        <Input
-                            id="domain"
-                            type="text"
-                            required
-                            tabIndex={1}
-                            autoComplete="domain"
-                            value={data.domain}
-                            onChange={(e) => setData('domain', e.target.value)}
-                            disabled={processing}
-                            placeholder="Domain"
-                        />
-                        <InputError message={errors.domain} className="mt-2" />
-                    </div>
+                    <div className="grid grid-cols-2 gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="website">Website</Label>
+                            <Input
+                                id="website"
+                                type="text"
+                                required
+                                tabIndex={2}
+                                autoComplete="website"
+                                value={data.website}
+                                onChange={(e) => setData('website', e.target.value)}
+                                disabled={processing}
+                                placeholder="Website"
+                            />
+                            <InputError message={errors.website} className="mt-2" />
+                        </div>
 
-                    <div className="grid gap-2">
-                        <Label htmlFor="name">Contact Person</Label>
-                        <Input
-                            id="name"
-                            type="text"
-                            required
-
-                            tabIndex={1}
-                            autoComplete="name"
-                            value={data.name}
-                            onChange={(e) => setData('name', e.target.value)}
-                            disabled={processing}
-                            placeholder="Contact Person"
-                        />
-                        <InputError message={errors.name} className="mt-2" />
+                        <div className="grid gap-2">
+                            <Label htmlFor="domain">Domain</Label>
+                            <Input
+                                id="domain"
+                                type="text"
+                                required
+                                tabIndex={3}
+                                autoComplete="domain"
+                                value={data.domain}
+                                onChange={(e) => setData('domain', e.target.value)}
+                                disabled={processing}
+                                placeholder="Domain"
+                            />
+                            <InputError message={errors.domain} className="mt-2" />
+                        </div>
                     </div>
 
-                    <div className="grid gap-2">
-                        <Label htmlFor="email">Email address</Label>
-                        <Input
-                            id="email"
-                            type="email"
-                            required
-                            tabIndex={2}
-                            autoComplete="email"
-                            value={data.email}
-                            onChange={(e) => setData('email', e.target.value)}
-                            disabled={processing}
-                            placeholder="email@example.com"
-                        />
-                        <InputError message={errors.email} />
+                    <div className="grid grid-cols-2 gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="name">Contact Person</Label>
+                            <Input
+                                id="name"
+                                type="text"
+                                required
+                                tabIndex={4}
+                                autoComplete="name"
+                                value={data.name}
+                                onChange={(e) => setData('name', e.target.value)}
+                                disabled={processing}
+                                placeholder="Contact Person"
+                            />
+                            <InputError message={errors.name} className="mt-2" />
+                        </div>
+
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email address</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                required
+                                tabIndex={5}
+                                autoComplete="email"
+                                value={data.email}
+                                onChange={(e) => setData('email', e.target.value)}
+                                disabled={processing}
+                                placeholder="email@example.com"
+                            />
+                            <InputError message={errors.email} />
+                        </div>
                     </div>
 
-                    <div className="grid gap-2">
-                        <Label htmlFor="password">Password</Label>
-                        <Input
-                            id="password"
-                            type="password"
-                            required
-                            tabIndex={3}
-                            autoComplete="new-password"
-                            value={data.password}
-                            onChange={(e) => setData('password', e.target.value)}
-                            disabled={processing}
-                            placeholder="Password"
-                        />
-                        <InputError message={errors.password} />
+                    <div className="grid grid-cols-2 gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="password">Password</Label>
+                            <Input
+                                id="password"
+                                type="password"
+                                required
+                                tabIndex={6}
+                                autoComplete="new-password"
+                                value={data.password}
+                                onChange={(e) => setData('password', e.target.value)}
+                                disabled={processing}
+                                placeholder="Password"
+                            />
+                            <InputError message={errors.password} />
+                        </div>
+
+                        <div className="grid gap-2">
+                            <Label htmlFor="password_confirmation">Confirm password</Label>
+                            <Input
+                                id="password_confirmation"
+                                type="password"
+                                required
+                                tabIndex={7}
+                                autoComplete="new-password"
+                                value={data.password_confirmation}
+                                onChange={(e) => setData('password_confirmation', e.target.value)}
+                                disabled={processing}
+                                placeholder="Confirm password"
+                            />
+                            <InputError message={errors.password_confirmation} />
+                        </div>
                     </div>
 
-                    <div className="grid gap-2">
-                        <Label htmlFor="password_confirmation">Confirm password</Label>
-                        <Input
-                            id="password_confirmation"
-                            type="password"
-                            required
-                            tabIndex={4}
-                            autoComplete="new-password"
-                            value={data.password_confirmation}
-                            onChange={(e) => setData('password_confirmation', e.target.value)}
-                            disabled={processing}
-                            placeholder="Confirm password"
-                        />
-                        <InputError message={errors.password_confirmation} />
-                    </div>
-
-                    <Button type="submit" className="mt-2 w-full" tabIndex={5} disabled={processing}>
+                    <Button type="submit" className="mt-2 w-full" tabIndex={8} disabled={processing}>
                         {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
                         Create account
                     </Button>
