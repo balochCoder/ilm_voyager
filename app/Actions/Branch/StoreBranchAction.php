90159fb431c6d7926e3cabc9bc1489916048b2e2
Raheel Ahmed <raheelahmed017@gmail.com>
2025-07-04 23:53:50 +0500

    wip

---
+++ app/Actions/Branch/StoreBranchAction.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Branch;
+
+use App\Enums\TenantRolesEnum;
+use App\Http\Requests\Branch\StoreBranchRequest;
+use App\Models\Branch;
+use App\Models\User;
+use Dflydev\DotAccessData\Data;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\DatabaseManager;
+
+final class StoreBranchAction
+{
+
+
+    public function __construct(
+        private DatabaseManager $db
+    ) {}
+
+    public function execute(StoreBranchRequest $request): Branch
+    {
+        $validated = $request->validated();
+        return $this->db->transaction(function () use ($validated) {
+            $user = User::create([
+                'name' => $validated['contact_name'],
+                'email' => $validated['user_email'],
+                'password' => Hash::make($validated['password']),
+                'designation' => $validated['designation'] ?? null,
+                'phone' => $validated['user_phone'] ?? null,
+                'mobile' => $validated['mobile'],
+                'whatsapp' => $validated['whatsapp'] ?? null,
+                'skype' => $validated['skype'] ?? null,
+                'download_csv' => $validated['download_csv'],
+            ]);
+            $user->assignRole(TenantRolesEnum::BRANCHOFFICE->value);
+
+            $branch = Branch::create([
+                'user_id' => $user->id,
+                'name' => $validated['name'],
+                'address' => $validated['address'] ?? null,
+                'city' => $validated['city'] ?? null,
+                'state' => $validated['state'] ?? null,
+                'country' => $validated['country'],
+                'time_zone_id' => $validated['time_zone_id'],
+                'phone' => $validated['phone'] ?? null,
+                'website' => $validated['website'] ?? null,
+                'email' => $validated['email'] ?? null,
+            ]);
+
+            return $branch;
+        });
+    }
+}
---
+++ app/Http/Requests/Branch/StoreBranchRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Branch;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreBranchRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            // Branch fields
+            'name' => 'required|string|max:255',
+            'address' => 'nullable|string|max:255',
+            'city' => 'nullable|string|max:255',
+            'state' => 'nullable|string|max:255',
+            'country' => 'required|string|max:255',
+            'time_zone_id' => 'required|ulid|exists:time_zones,id',
+            'phone' => 'nullable|string|max:255',
+            'website' => 'nullable|string|max:255',
+            'email' => 'nullable|email|max:255',
+            // User fields
+            'contact_name' => 'required|string|max:255',
+            'user_email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+            'designation' => 'nullable|string|max:255',
+            'user_phone' => 'nullable|string|max:255',
+            'mobile' => 'required|string|max:255',
+            'whatsapp' => 'nullable|string|max:255',
+            'skype' => 'nullable|string|max:255',
+            'download_csv' => ['required', Rule::in(['allowed', 'allowed_without_contact', 'not_allowed'])],
+        ];
+    }
+}
