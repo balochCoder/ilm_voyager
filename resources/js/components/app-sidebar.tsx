eb9a8c182934ac20705d9371636cb9d8090f43be
Raheel Ahmed <raheelahmed017@gmail.com>
2025-07-05 17:23:51 +0500

    wip

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -3,7 +3,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarGroup, SidebarGroupLabel, SidebarMenuSub, SidebarMenuSubItem, SidebarMenuSubButton } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
-import { LayoutGrid, ChevronRight, Globe2, BuildingIcon, Users, BookOpen, Settings, MapPin, Globe } from 'lucide-react';
+import { LayoutGrid, ChevronRight, Globe2, BuildingIcon, MapPin, Globe } from 'lucide-react';
 import AppLogo from './app-logo';
 import { Collapsible, CollapsibleTrigger, CollapsibleContent } from '@/components/ui/collapsible';
 import { usePermission } from '@/hooks/use-permission';
